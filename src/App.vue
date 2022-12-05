fd5387d11357110f7052e73293105691ec18920e
ElRafo92 <rafael.martinez@outlook.it>
2022-12-05 13:10:23 +0100

    add header components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
+    
+    <AppHeader />
+    <AppHero />
+    <AppMain />
+    <AppFooter />
 
-  </div>
 </template>
 
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppHero from './components/AppHero.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
   export default {
-    
+    name: "App",
+    components: {
+      AppHeader,
+      AppHero,
+      AppMain,
+      AppFooter,
+    },
   }
 </script>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Logo />
+        <NavBar />
+    </div>
+</template>
+
+<script>
+    import Logo from './Logo.vue';
+    import NavBar from './NavBar.vue'
+    export default {
+        name: "AppHeader",
+        components: {
+            Logo,
+            NavBar,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
